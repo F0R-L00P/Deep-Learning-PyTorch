b293f6b41159f8aeab867a005738a162aa139ac8
F0R-L00P <67719682+F0R-L00P@users.noreply.github.com>
2023-01-25 14:01:57 -0500

    20230125

--- 3.mnist/image_classification.py
+++ 3.mnist/image_classification.py
@@ -46,6 +46,7 @@ test_loader = DataLoader(
                     ) 
 
 # building network model
+# when initializing the class the architecture gets initalized first
 class NeuralNet(nn.Module):
     def __init__(self, input_layer, hidden_layer, output_layer):
         super(NeuralNet, self).__init__()
@@ -62,36 +63,82 @@ class NeuralNet(nn.Module):
 
         return output_var3
 
-#Create an object of the class, which represents our network 
+# Create an object of the class, which represents our network 
 net = NeuralNet(input_size, hidden_size, out_size)
+# view network architecture and layer
+print(net.get_parameter)
+# acossiated layer weight
+print(list(net.parameters()))
+
 CUDA = torch.cuda.is_available()
+# if GPU is available then switch tensors on GPU
 if CUDA:
     net = net.cuda()
-#The loss function. The Cross Entropy loss comes along with Softmax. Therefore, no need to specify Softmax as well
+# The loss function. The Cross Entropy loss comes along with Softmax. 
+# Therefore, no need to specify Softmax as well
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-#Train the network
+##############################
+# VISUALIZING the train_loader
+#############################
+for i, (images, label) in enumerate(train_loader):
+    print(images.size())
+    images = images.view(-1, 784) # this will flatten out the image
+    print(images.size())
+
+
+###################
+# TRAIN the network
+###################
+# training process must be defined step by step
+
+# first loop is epoch
 for epoch in range(epochs):
     correct_train = 0
     running_loss = 0
+    # second loop is iteration
+    # each iteration takes batch of 100 and applies the following
     for i, (images, labels) in enumerate(train_loader):   
-        #Flatten the image from size (batch,1,28,28) --> (100,1,28,28) where 1 represents the number of channels (grayscale-->1),
+        # Flatten the image from size (batch,1,28,28) --> (100,1,28,28) 
+        # where 1 represents the number of channels (grayscale-->1),
         # to size (100,784) and wrap it in a variable
         images = images.view(-1, 28*28)    
         if CUDA:
             images = images.cuda()
             labels = labels.cuda()
-            
-        outputs = net(images)       
-        _, predicted = torch.max(outputs.data, 1)                                              
-        correct_train += (predicted == labels).sum() 
-        loss = criterion(outputs, labels)                 # Difference between the actual and predicted (loss function)
-        running_loss += loss.item()
+        # clear gradient
         optimizer.zero_grad() 
+        outputs = net(images)                             # complete forward pass      
+        _, predicted = torch.max(outputs.data, 1)         # max provides index of highest value per row                              
+        correct_train += (predicted == labels).sum()      # compare 2 tensos, sums number of the index equal values
+        loss = criterion(outputs, labels)                 # Difference between the actual and predicted (loss function)
+        running_loss += loss.item()                       # loss.item() fetchs tensor value
         loss.backward()                                   # Backpropagation
         optimizer.step()                                  # Update the weights
         
     print('Epoch [{}/{}], Training Loss: {:.3f}, Training Accuracy: {:.3f}%'.format
-          (epoch+1, epochs, running_loss/len(train_loader), (100*correct_train.double()/len(train_dataset))))
+          (epoch+1, epochs, running_loss/len(train_loader), (100*correct_train.double()/len(train_data))))
 print("DONE TRAINING!")
+
+# suming EXAMPLE
+t1 = torch.tensor([1, 2, 3, 4])
+t2 = torch.tensor([1, 8, 8, 4])
+print(t1 == t2)
+print((t1 == t2).sum())
+
+###################
+# TEST the network
+###################
+with torch.no_grad():
+    correct = 0
+    for images, labels in test_loader:
+        if CUDA:
+            images = images.cuda()
+            labels = labels.cuda()
+        images = images.view(-1, 28*28)
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / len(test_data)))
