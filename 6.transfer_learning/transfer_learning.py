b26db75a30fccb47dfd07cc9ff39b416b674bbb0
F0R-L00P <67719682+F0R-L00P@users.noreply.github.com>
2023-03-24 12:23:59 -0400

    20230324

--- 6.transfer_learning/transfer_learning.py
+++ 6.transfer_learning/transfer_learning.py
@@ -17,3 +17,13 @@ import numpy as np
 #       -> image net has 1000 classes, your data has 5
 #       -> change last layer to reflect 5
 ##########################################################
+# to normalize input:
+# --> (input[channel] - mean[channel]) / std[channel]
+data_tranforms = {
+    'train': transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
