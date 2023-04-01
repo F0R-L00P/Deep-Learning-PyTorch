430468ecc859b73b66cf415ffad74af056e5403e
F0R-L00P <67719682+F0R-L00P@users.noreply.github.com>
2023-04-01 14:51:36 -0400

    20230401

--- 12.rnn/lstm_text_generation.py
+++ 12.rnn/lstm_text_generation.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.nn.utils import clip_grad
+from torch.nn.utils import clip_grad_norm_
 
 import os
 import string
@@ -116,12 +116,14 @@ tensor_data = corpus.get_data(full_path, batch_size)
 print(tensor_data.shape)
 
 # obtain vocab size 
-vocab_size = len(corpus.dictionary)
+vocab_size = len(corpus.dictionary.word2index)
 print(vocab_size)
 
 #########################################################################
 #########################################################################
 #########################################################################
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class TextGenerator(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, num_layers=1):
         super(TextGenerator, self).__init__()
@@ -142,15 +144,115 @@ class TextGenerator(nn.Module):
         # Pass the LSTM output through the linear layer
         x = self.linear(x)
         return x, hidden
+    
+def detach(states):
+    """
+If we have a tensor z,'z.detach()' returns a tensor that shares the same storage
+as 'z', but with the computation history forgotten. It doesn't know anything
+about how it was computed. In other words, we have broken the tensor z away from its past history
+Here, we want to perform truncated Backpropagation
+TBPTT splits the 1,000-long sequence into 50 sequences (say) each of length 20 and treats each sequence of length 20 as 
+a separate training case. This is a sensible approach that can work well in practice, but it is blind to temporal 
+dependencies that span more than 20 timesteps.
+    """
+    return [state.detach() for state in states] 
 
-# setting parameters
-embed_size = 128    #Input features to the LSTM
-hidden_size = 1024  #Number of LSTM units
-num_layers = 1
-num_epochs = 20
-batch_size = 20
-timesteps = 30 # look at 30 previouse words to predict the next word
-learning_rate = 0.002
+# setting variable parametes
+# Sample variables
+vocab_size = vocab_size # Assuming there are 100 unique words in the text
+embed_size = 128        # Embedding size
+hidden_size = 1024      # Hidden state size
+num_layers = 1          # Number of LSTM layers
+batch_size = 20         # Batch size
+num_epochs = 10         # number of epochs
+timesteps = 30          # Sequence length  
+learning_rate=0.002
+
 
 # initalize class
-lstm_model = TextGenerator()
+model = TextGenerator(vocab_size, embed_size, hidden_size, vocab_size, num_layers)
+model = model.to(device)
+
+# setup loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+patience = 5
+num_epochs_without_improvement = 3
+min_val_loss = float('inf')
+
+for epoch in tqdm(range(num_epochs)):
+    epoch_loss = 0
+
+    for i in range(0, tensor_data.size(1) - timesteps, timesteps):
+        # Get mini-batch inputs and targets
+        inputs = tensor_data[:, i:i+timesteps].to(device)
+        targets = tensor_data[:, i:i+timesteps].to(device)
+
+        # Set initial hidden and cell states
+        input_batch_size = inputs.size(0)
+        states = (torch.zeros(num_layers, input_batch_size, hidden_size).to(device),
+                  torch.zeros(num_layers, input_batch_size, hidden_size).to(device))
+
+        # Zero the gradients
+        optimizer.zero_grad()
+        # Forward pass
+        outputs, states = model(inputs, states)
+        # Detach states to prevent backpropagation through entire sequence history
+        states = detach(states)
+        # Calculate the loss
+        loss = criterion(outputs, targets.reshape(-1))
+        epoch_loss += loss.item()
+        # Backward pass
+        loss.backward()
+        # Clip gradients to prevent exploding gradients
+        clip_grad_norm_(model.parameters(), 0.5)
+        # Update the weights
+        optimizer.step()
+
+        step = (i + 1) // timesteps
+        if step % 100 == 0:
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Step [{step}], Loss: {loss.item()}")
+
+    # Calculate the average loss for the current epoch
+    avg_epoch_loss = epoch_loss / (i + 1)
+
+    # Check if the validation loss has improved
+    if avg_epoch_loss < min_val_loss:
+        min_val_loss = avg_epoch_loss
+        num_epochs_without_improvement = 0
+    else:
+        num_epochs_without_improvement += 1
+
+    # Check if early stopping should be applied
+    if num_epochs_without_improvement >= patience:
+        print("Early stopping applied.")
+        break
+
+
+# Test the model
+with torch.no_grad():
+    with open('results.txt', 'w') as f:
+        # Set initial hidden and cell states
+        state = (torch.zeros(num_layers, 1, hidden_size).to(device),
+                 torch.zeros(num_layers, 1, hidden_size).to(device))
+        # Select one word id randomly and convert it to shape (1,1)
+        inputs = torch.randint(0, vocab_size, (1,)).long().unsqueeze(1).to(device)
+
+        for i in range(500):
+            output, _ = model(inputs, state)
+            print(output.shape)
+            # Sample a word id from the exponential of the output 
+            prob = output.exp()
+            word_id = torch.multinomial(prob, num_samples=1).item()
+            print(word_id)
+            # Replace the input with sampled word id for the next time step
+            inputs.fill_(word_id)
+
+            # Write the results to file
+            word = corpus.dictionary.index2word[word_id]
+            word = '\n' if word == '<eos>' else word + ' '
+            f.write(word)
+            
+            if (i+1) % 100 == 0:
+                print('Sampled [{}/{}] words and save to {}'.format(i+1, 500, 'results.txt'))
